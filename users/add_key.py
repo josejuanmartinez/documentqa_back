e66f897d47ddd71cdbfef50fdd74ff590171a056
Jose J. Martinez <jose.martinez@wayops.eu>
2023-04-18 10:46:53 +0100

    Working in docker

--- users/add_key.py
+++ users/add_key.py
@@ -4,24 +4,25 @@ import sys
 import keyring
 
 SERVICE_ID = "sintentic_qa"
+USAGE = 'add_key.py -e <email> -p <password>'
 
 
 def main(argv):
-    opts, args = getopt.getopt(argv, "he:p:", ["email=", "password="])
+    opts, args = getopt.getopt(argv, "he:p:", [])
     email = None
     password = None
     for opt, arg in opts:
         if opt == '-h':
-            print('add_key.py -e <email> -p <password>')
+            print(USAGE)
             sys.exit()
-        elif opt in ("-e", "--email"):
+        elif opt == "-e":
             email = arg
-        elif opt in ("-p", "--password"):
+        elif opt == "-p":
             password = arg
     if email is not None and password is not None:
         keyring.set_password(SERVICE_ID, email, password)
     else:
-        print('add_key.py -e <email> -p <password>')
+        print(USAGE)
         sys.exit()
 
 
